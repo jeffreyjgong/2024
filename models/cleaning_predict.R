c84125eb5fbcf935e1608d2cf47b3f32b3c6761b
johnliu2003 <123904853+johnliu2003@users.noreply.github.com>
2024-10-24 12:49:02 -0400

    Added voting percentages

--- models/cleaning_predict.R
+++ models/cleaning_predict.R
@@ -8,27 +8,29 @@ library(tidyr)
 library(dplyr)
 library(lubridate)
 
+
+#### Call Times ####
+# Predict call times (in minutes) for Arizona, Georgia, Michigan, Nevada, 
+# North Carolina, Pennsylvania, and Wisconsin.
+
+predict_states <- c("Arizona", "Georgia", "Michigan", "Nevada", "North Carolina", 
+                    "Pennsylvania", "Wisconsin")
+
 calltimes <- read_html("https://www.270towin.com/2020-election-results-live/")
-
-
 
 calltimes_state <- calltimes %>% 
   html_nodes(xpath = '//*[(@id = "live_results_presidential_stats_wrapper")]//*[contains(concat( " ", @class, " " ), concat( " ", "border-left", " " ))]') %>% 
   html_text()
 
-# join calltimes_state and blank column
 calltimes_df <- data.frame(calltimes_state, rep("", 58))
-# remove first and last row
 calltimes_df <- calltimes_df[-c(1,58), ]
 
-# separate character string in calltimes_df$calltimes_state
+
 calltimes_df <- calltimes_df %>% 
   separate(calltimes_state, into = c("a", "State", "Time"), sep = "\n") %>% 
   select(State, Time)
-
 calltimes_df$Date <- ifelse(grepl("/", calltimes_df$Time), trimws(substr(calltimes_df$Time, 1, 5)), "11/3")
 calltimes_df$Time <- ifelse(grepl("/", calltimes_df$Time), trimws(substr(calltimes_df$Time, 6, nchar(calltimes_df$Time))), calltimes_df$Time)
-
 
 poll_closing_times <- data.frame(
   State = c("Georgia", "Indiana", "Kentucky", "South Carolina", "Vermont", "Virginia", 
@@ -64,13 +66,37 @@ calltimes_df$poll_closing_time <- as.POSIXct(paste0("2020/11/03 ", calltimes_df$
 calltimes_df$call_time <- as.POSIXct(calltimes_df$call_time, format = "%Y/%m/%d %I:%M %p")
 calltimes_df$poll_closing_time <- as.POSIXct(calltimes_df$poll_closing_time, format = "%Y/%m/%d %I:%M %p")
 # Calculate the difference between the call time and the poll closing time
-calltimes_df$diff_2020 <- calltimes_df$call_time - calltimes_df$poll_closing_time
+calltimes_df$time_diff_2020 <- calltimes_df$call_time - calltimes_df$poll_closing_time
 
 calltimes_df <- calltimes_df %>% 
-  select(State, Clean_State, call_time, poll_closing_time, diff_2020)
+  select(State, Clean_State, call_time, poll_closing_time, time_diff_2020)
+
+calltimes_df$Clean_State[calltimes_df$Clean_State == "Washington, DC"] <- "District of Columbia"
+
+calltimes_df$Clean_State <- toupper(calltimes_df$Clean_State)
 
 # View the result
 head(calltimes_df)
 
-write.csv(calltimes_df, file = "data/calltimes_df.csv")
+# write.csv(calltimes_df, file = "data/calltimes_df.csv")
+
+pres_df <- read.csv("data/1976-2020-president.csv")
+head(pres_df)
+
+pres_df_2020 <- pres_df %>% 
+  filter(year == 2020) %>% 
+  select(state, state_po, candidate, candidatevotes, totalvotes) %>% 
+  mutate(vote_percent = candidatevotes / totalvotes)
+
+# output difference between top two vote_percent by state
+# keep top two vote_percent by state
+pres_df_2020 <- pres_df_2020 %>% 
+  group_by(state) %>% 
+  arrange(desc(vote_percent)) %>% 
+  mutate(vote_diff = first(vote_percent) - nth(vote_percent, 2)) %>% 
+  filter(row_number() <= 1)
+
+calltimes_df <- calltimes_df %>% 
+  right_join(pres_df_2020, by = c("Clean_State" = "state"))
+
 
